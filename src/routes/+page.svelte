4c2e096d556f3ae0da38b78acc7d725fc3ec0703
watchlar2000 <sergii.koch@gmail.com>
2025-01-07 21:29:09 +0100

    generate project

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	let { data } = $props();
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<pre>{JSON.stringify(data, null, 2)}</pre>
