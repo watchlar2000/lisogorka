cb462a6438674ceb2c880ea5d04c679ee23dd31c
watchlar2000 <sergii.koch@gmail.com>
2025-02-03 21:12:32 +0100

    feat: update auth service

--- src/lib/server/api/configs/env.dto.ts
+++ src/lib/server/api/configs/env.dto.ts
@@ -4,6 +4,7 @@ export const envsDTO = z.object({
 	DATABASE_URL: z.string(),
 	GOOGLE_CLIENT_ID: z.string(),
 	GOOGLE_CLIENT_SECRET: z.string(),
+	ALLOWED_GOOGLE_EMAIL: z.string().email(),
 });
 
 export type EnvsDTO = z.infer<typeof envsDTO>;
